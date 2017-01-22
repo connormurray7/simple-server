054326629bf4fa682fe1c4f9644341730efab2ef
Connor Murray <connormurray7@gmail.com>
2017-01-21 19:32:23 -0600

    breaking up listening socket more

--- simple-server/ListeningSocket.cpp
+++ simple-server/ListeningSocket.cpp
@@ -16,19 +16,24 @@ ListeningSocket::ListeningSocket(std::string local_addr, std::string port) {
 int ListeningSocket::get_local_socket() {
     struct addrinfo *addr;
     struct addrinfo hints;
-    
-    /* open a TCP socket */
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = PF_UNSPEC; /* any supported protocol */
-    hints.ai_flags = AI_PASSIVE; /* result for bind() */
-    hints.ai_socktype = SOCK_STREAM; /* TCP */
-    int error = getaddrinfo(local_addr.c_str(), port.c_str(), &hints, &addr);
-    if (error) {
-        errx(1, "getaddrinfo failed: %s", gai_strerror(error));
-    }
-    
+    setup_hints(hints);
+
+    // open a TCP socket
     int local_socket = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
     bind(local_socket, addr->ai_addr, addr->ai_addrlen);
     listen(local_socket, 5);
     return local_socket;
 }
+
+void setup_hints(struct addrinfo& hints) {
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = PF_UNSPEC; //all protocols
+    hints.ai_flags = AI_PASSIVE;
+    hints.ai_socktype = SOCK_STREAM; //tcp
+    int error = getaddrinfo(local_addr.c_str(), port.c_str(), &hints, &addr);
+    if (error) {
+        errx(1, "getaddrinfo failed: %s", gai_strerror(error));
+    }
+}
+    
+
--- simple-server/Poller.h
+++ simple-server/Poller.h
@@ -2,37 +2,41 @@
 
 //#include <folly/MPMCQueue.h>
 
+///Poller listens to a local socket and offloads any 
+///incoming request to a queue (a folly multi-producer, multi-
+///consumser queue) so that each request can be dealt with
+///with a threadpool or some other type of consumer.
 class Poller {
-	//Poller listens to a local socket and offloads any 
-	//incoming request to a queue (a folly multi-producer, multi-
-	//consumser queue) so that each request can be dealt with
-	//with a threadpool or some other type of consumer.
 public:
+
+    ///Main driver for the server. Blocks forever
+    ///unless interrupted. 
     virtual void loop_forever(int local_socket) = 0;
-        //Main driver for the server. Blocks forever
-        //unless interrupted. 
 };
 
-class KQueuePoller : public Poller {
-    //FreeBSD implementation of a Poller.
-    //Uses kqueue(2) to queue requests.
-public:
-    KQueuePoller();
-        //Empty Poller.
 
+///FreeBSD implementation of a Poller.
+///Uses kqueue(2) to queue requests.
+class KQueuePoller : public Poller {
+public:
+
+    ///Empty Poller.
+    KQueuePoller();
+
+    ///Monitors local_socket, blocks 
+    ///unless interrupted.
     void loop_forever(int local_socket);
-        //Monitors local_socket, blocks 
-        //unless interrupted.
 };
 
 class EPollPoller : public Poller {
-    //Linux implementation of a Poller.
-    //Uses epoll(7) to queue requests.
+    ///Linux implementation of a Poller.
+    ///Uses epoll(7) to queue requests.
 public:
-    EPollPoller();
-        //Empty Poller.
 
+    ///Empty Poller.
+    EPollPoller();
+
+    ///Monitors local_socket, blocks 
+    ///unless interrupted.
     void loop_forever(int local_socket);
-        //Monitors local_socket, blocks 
-        //unless interrupted.
 };
