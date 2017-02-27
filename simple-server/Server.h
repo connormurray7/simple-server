482b8512b4963fd825502ecaf2bff4aab7079fe9
Connor Murray <connormurray7@gmail.com>
2017-02-26 18:47:11 -0600

    static assert with type traits for server

--- simple-server/Server.h
+++ simple-server/Server.h
@@ -3,20 +3,36 @@
 #include <string>
 #include <memory>
 #include <thread>
+#include <type_traits>
 #include <folly/MPMCQueue.h>
 
+#include "RequestHandler.h"
 #include "ListeningSocket.h"
 #include "Poller.h"
 #include "Dequeuer.h"
 #include "Request.h"
 
+///Top level class of this project. Requires a type T, that extends the
+///abstract class RequestHandler. EchoHandler is given as an example,
+///other implementations could include parsing an HTTP message and 
+///writing back another HTTP message based on your use-case.
 template<typename T>
 class Server {
+static_assert(std::is_base_of<RequestHandler, T>::value, "T must inherit from RequestHandler");
 public:
 
+    ///Requires a local address and port. Listens to a socket 
+    ///created at this address. Also takes an optional number of
+    ///threads (default 1) that will be used in a threadpool to handle events.
     Server(std::string addr, std::string port, int num_threads)
         : address(addr), port(port), num_threads(num_threads) {}
 
+    Server(std::string addr, std::string port)
+        : address(addr), port(port), num_threads(1) {}
+
+    ///Blocking call, this function will not return unless
+    ///an exception is thrown when trying to listen to the 
+    ///address and port given.
     void run();
 
 private:
