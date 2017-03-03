098aa243ce4ff06a93af15ffbc3c37243916ab00
Connor Murray <connormurray7@gmail.com>
2017-03-02 23:11:19 -0600

    dequeuer

--- test/test_dequeuer.cpp
+++ test/test_dequeuer.cpp
@@ -1,15 +1,19 @@
 #include <string>
 #include <iostream>
+#include <memory>
 #include <folly/MPMCQueue.h>
 
 #include "../simple-server/Request.h"
 #include "../simple-server/Response.h"
 #include "../simple-server/RequestHandler.h"
+#include "../simple-server/Dequeuer.h"
 #include "catch.hpp"
 
 using std::cout;
 using std::endl;
 using std::string;
+using std::shared_ptr;
+using std::make_shared;
 using folly::MPMCQueue;
 
 class TestRequestHandler : public RequestHandler {
@@ -25,11 +29,16 @@ public:
 };
 
 TEST_CASE("Dequeuer handles requests on queue", "[Server]") {
-    MPMCQueue<Request> queue = MPMCQueue<Request>(16);
+    auto queue = std::make_shared<folly::MPMCQueue<Request>>(1024);
 
     for(int i = 0; i < 10; ++i) {
         Request request(0, "request 0");
-        queue.blockingWrite(std::forward<Request>(request));
+        queue->blockingWrite(std::forward<Request>(request));
     }
 
+    auto handler = shared_ptr<TestRequestHandler>(new TestRequestHandler());
+    auto dequeuer = Dequeuer(queue, handler, 1);
+
+    dequeuer.begin();
+    cout << "This returns" <<endl;
 }
