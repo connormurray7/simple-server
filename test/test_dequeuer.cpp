8b1b2e03003d54aba017df7284bfcf6586d087f0
Connor Murray <connormurray7@gmail.com>
2017-03-04 23:10:20 -0600

    negative test dequeuer

--- test/test_dequeuer.cpp
+++ test/test_dequeuer.cpp
@@ -39,3 +39,18 @@ TEST_CASE("Dequeuer handles requests on queue", "[Server]") {
     dequeuer.begin();
     REQUIRE(queue->isEmpty() <= 0); 
 }
+
+TEST_CASE("Dequeuer with zero threads don't dequeue", "[Server]") {
+    auto queue = std::make_shared<folly::MPMCQueue<Request>>(1024);
+
+    for(int i = 0; i < 10; ++i) {
+        Request request(i, "request " + std::to_string(i));
+        queue->blockingWrite(std::forward<Request>(request));
+    }
+
+    auto handler = shared_ptr<TestRequestHandler>(new TestRequestHandler());
+    Dequeuer dequeuer(queue, handler, 0);
+
+    dequeuer.begin();
+    REQUIRE(!queue->isEmpty()); 
+}
